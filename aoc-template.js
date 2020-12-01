eea1afe43f530603d15669270c778a53d5d1ed90
Timothée Fermeaux <fermeaux.t@gmail.com>
2020-12-01 06:23:28 +0100

    initialize environment

---
+++ aoc-template.js
@@ -0,0 +1,15 @@
+function solution1(input) {
+    return ''
+}
+
+function solution2(input) {
+    return ''
+}
+
+const input = ''
+
+const value1 = solution1(input)
+const value2 = solution2(input)
+
+console.log(value1)
+console.log(value2)
