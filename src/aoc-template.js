f39befd396d37716dde00e081f01e23c81602e8f
Timothée Fermeaux <fermeaux.t@gmail.com>
2020-12-07 07:31:54 +0100

    improve template to support test input

--- src/aoc-template.js
+++ src/aoc-template.js
@@ -6,10 +6,16 @@ function solution2(input) {
     return ''
 }
 
+const testInput = ``
 const input = ``
-
 const value1 = solution1(input)
+const value1Test = solution1(testInput)
 const value2 = solution2(input)
+const value2Test = solution2(testInput)
 
+console.log('===== Test Output =====')
+console.log(value1Test)
+console.log(value2Test)
+console.log('=====  My Output  =====')
 console.log(value1)
 console.log(value2)
